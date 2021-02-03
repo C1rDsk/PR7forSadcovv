b83c19b8157cba5529b3d9ad2920177730b662c4
Александр Рязанов <sanderj29@gmail.com>
2021-02-03 13:22:06 +0300

    Добавьте файлы проекта.

---
+++ protect_inf_LR1/Form1.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Numerics;
+
+namespace protect_inf_LR1
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonEncrypt_Click(object sender, EventArgs e)
+        {
+            if ((textBox_p.Text.Length > 0) && (textBox_q.Text.Length > 0))
+            {
+                long p = Convert.ToInt64(textBox_p.Text);
+                long q = Convert.ToInt64(textBox_q.Text);
+
+                if (IsTheNumberSimple(p) && IsTheNumberSimple(q))
+                {
+                    string str = textBox1.Text;
+
+                    long n = p * q;
+                    long m = (p - 1) * (q - 1);
+                    long d = Calculate_d(m);
+                    long e_ = Calculate_e(d, m);
+
+                    List<string> result = RSA_Endoce(str, e_, n);
+
+                    StreamWriter sw = new StreamWriter("out1.txt");
+                    foreach (string item in result)
+                        sw.WriteLine(item);
+                    sw.Close();
+
+                    textBox_d.Text = d.ToString();
+                    textBox_n.Text = n.ToString();
+
+                    Process.Start("out1.txt");
+                }
+                else
+                    MessageBox.Show("p или q - не простые числа!");
+            }
+            else
+                MessageBox.Show("Введите p и q!");
+        }
+
+        //расшифровать
+        private void buttonDecipher_Click(object sender, EventArgs e)
+        {
+            if ((textBox_d.Text.Length > 0) && (textBox_n.Text.Length > 0))
+            {
+                long d = Convert.ToInt64(textBox_d.Text);
+                long n = Convert.ToInt64(textBox_n.Text);
+
+                List<string> input = new List<string>();
+
+                StreamReader sr = new StreamReader("out1.txt");
+
+                while (!sr.EndOfStream)
+                {
+                    input.Add(sr.ReadLine());
+                }
+
+                sr.Close();
+
+                string result = RSA_Decode(input, d, n);
+
+                StreamWriter sw = new StreamWriter("out2.txt");
+                sw.WriteLine(result);
+                sw.Close();
+
+                Process.Start("out2.txt");
+            }
+            else
+                MessageBox.Show("Введите секретный ключ!");
+        }
+
+        //проверка: простое ли число?
+        private bool IsTheNumberSimple(long n)
+        {
+            if (n < 2)
+                return false;
+
+            if (n == 2)
+                return true;
+
+            for (long i = 2; i < n / 2; i++)
+                if (n % i == 0)
+                    return false;
+
+            return true;
+        }
+
+        //зашифровать
+        private List<string> RSA_Endoce(string s, long e, long n)
+        {
+            List<string> result = new List<string>();
+
+            BigInteger bi;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                int index =  s[i];
+
+                bi = new BigInteger(index);
+                bi = BigInteger.Pow(bi, (int)e);
+
+                BigInteger n_ = new BigInteger((int)n);
+
+                bi = bi % n_;
+
+                result.Add(bi.ToString());
+                textBox2.Text += bi.ToString()+ "\r\n";
+            }
+
+            return result;
+        }
+
+        //расшифровать
+        private string RSA_Decode(List<string> input, long d, long n)
+        {
+            string result = "";
+
+            BigInteger bi;
+
+            foreach (string item in input)
+            {
+                bi = new BigInteger(Convert.ToDouble(item));
+                bi = BigInteger.Pow(bi, (int)d);
+
+                BigInteger n_ = new BigInteger((int)n);
+
+                bi = bi % n_;
+
+                int index = Convert.ToInt32(bi.ToString());
+
+                result += (char)index;
+            }
+
+            return result;
+        }
+
+        //вычисление параметра d. d должно быть взаимно простым с m
+        private long Calculate_d(long m)
+        {
+            long d = m - 1;
+
+            for (long i = 2; i <= m; i++)
+                if ((m % i == 0) && (d % i == 0)) //если имеют общие делители
+                {
+                    d--;
+                    i = 1;
+                }
+
+            return d;
+        }
+
+        //вычисление параметра e
+        private long Calculate_e(long d, long m)
+        {
+            long le = 10;
+
+            while (true)
+            {
+                if ((le * d) % m == 1)
+                    break;
+                else
+                    le++;
+            }
+
+            return le;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            List<string> s = new List<string>();
+            StreamReader sr = new StreamReader("in.txt");
+            int i = 0;
+            while (!sr.EndOfStream)
+            {
+                s.Add(sr.ReadLine());
+                s[i] = s[i].ToUpper();
+                i++;
+            }
+            foreach (string line in s)
+            {
+                textBox1.Text += line + "\r\n";
+            }
+
+            sr.Close();
+        }
+    }
+}
